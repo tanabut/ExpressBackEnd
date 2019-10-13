becbe3e941ca57461aa2ad15522f0909c69638fd
tanabutcha <tanabut.cha@ku.th>
2019-10-13 22:56:08 +0700

    Update port server

--- server.js
+++ server.js
@@ -53,4 +53,17 @@ app.get('/fiderestaurants', function(req,res){
 	)
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// port (as described above) and host are both wrong
+//const host = 'localhost';
+//const port = 3000;
+
+// use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+	console.log("Server started.......");
+});
+
+
+//app.listen(3000, () => console.log('Example app listening on port 3000!'));
