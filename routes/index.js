f4ab2aabaea8bfaaa23b694b8e040bdb707c5561
tanabutcha <tanabut.cha@ku.th>
2019-10-13 23:02:14 +0700

    update routes linemessage

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const { sequencenumber, placeapi } = require('../controllers');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('API is working'));
+router.get('/', (req, res) => res.status(200).send('API is working'));
 router.get('/sequences', sequencenumber.getresults);
 router.get('/SCG', placeapi.fiderestaurants);
+router.post('/linemessage', (req, res) => res.sendStatus(200))
 
 module.exports = router;
